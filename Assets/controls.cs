a747238d7e4bcb9f2b4f64f165f38b4b0340c492
kasradzenika <kasradzenika@yahoo.com>
2014-10-20 16:00:30 +0400

    Basic Controls and Physics

--- Assets/controls.cs
+++ Assets/controls.cs
@@ -5,21 +5,18 @@ using System.Collections;
 
 public class controls : MonoBehaviour {
 
-
-	// Use this for initialization
-
-	void Start () {
-	
-
-		
-	}
-
-	
-	// Update is called once per frame
+    public float moveSpeed;
+    public float maxSpeed;
 
 	void Update () {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
 
-		
+        Vector2 dir = (Vector2.right * hor + Vector2.up * ver) * moveSpeed;
+
+        if(rigidbody2D.velocity.magnitude < maxSpeed){
+            rigidbody2D.AddForce(dir);
+        }
 	}
 
 }
